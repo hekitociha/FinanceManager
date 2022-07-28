0b312689a8568fa87f9b6501d11b2a88f824f505
hekitociha <chernov.200125@gmail.com>
2022-07-28 20:51:04 +0300

    навбар готов (наверное)

--- Front/src/Components/Header.js
+++ Front/src/Components/Header.js
@@ -1,39 +1,30 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, FormControl, Container, Form, Button  } from 'react-bootstrap'
-import logo from './money.jpg'
 import './Style.css'
 
 export default class Header extends Component {
     render() {
         return (
-            <Navbar className="navbar">
-                <Container className="container">
-                    <Navbar.Brand href="/">
-                        <img
-                            src={logo }
-                            height="30"
-                            width="30"
-                            className = "logo"
+            <div className="navbar">
+                <div className="navbar left">
+                    <a href="/"><img className="nav-img" src="../money.png" width="70" alt="ico"></img></a>
+                    <a className="nav-link" href="/">Главная</a>
+                    <a className="nav-link" href="/contacts" >Контакты</a>
+                </div>
+                <div className="navbar center">
+                    <Form className="search">
+                        <FormControl
+                            type="text"
+                            placeholder="Поиск"
+                            className="search"
                         />
-                    </Navbar.Brand>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className ="menu">
-                            <Nav.Link href="/"> Главная </Nav.Link>
-                            <Nav.Link href="/login"> Войти </Nav.Link>
-                            <Nav.Link href="/signup"> Зарегистрироваться </Nav.Link>
-                            <Nav.Link href="/contacts"> Контакты </Nav.Link>
-                        </Nav>
-                        <Form inline >
-                            <FormControl
-                                type="text"
-                                placeholder="Поиск"
-                                className="search"
-                            />
-                            <Button className="button"> Поиск </Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+                    </Form>
+                </div>
+                <div className="navbar right">
+                    <button className="button signin" onClick>Войти</button>
+                    <button className="button" onClick>Зарегистрироваться</button>
+                </div>
+            </div>
             )
     }
 }
